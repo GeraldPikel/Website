c0db72dd6db281aad5fa49645192377417b127a5
Gerald <geraldpikel6969@gmail.com>
2025-06-12 16:53:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-choice = input("YOU WAKE UP IN A FOREST ")
-print("YOU " + choice)
-print("MORE FOREST")
+footprint = 0
+
+has_pet = input("Do you have a pet (yes/no)? ")
+if has_pet == "yes":
+    # Pets consume resources like water, litter, and toys. 
+    footprint = footprint + 5
+    if input("Does your pet food contain meat?") == "yes":
+        footprint = footprint + 10        
+    else:
+        footprint = footprint + 0
+else:
+    footprint = footprint + 0
+
+
+days = int(input("How many days a week do you commute to school or work? "))
+transport = input("Do you commute by foot, bike, bus, train, or car? ")
+
+# Different methods of transportation have different environmental impacts.
+if transport == "car":
+    footprint = footprint + (8 * days)
+
+print("Your environmental footprint score is " + str(footprint) + ".")
